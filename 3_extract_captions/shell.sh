cb1da8480bea83a3f742cdb40de26a52f56dfb98
wuyihong <1300941320@qq.com>
2020-04-15 03:34:04 +0800

    add pdffigures1.0

---
+++ 3_extract_captions/shell.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# src directory of pdf files
+src_path="../../vis_data/papers"
+dst_path="./precomputed"
+ 
+for file in ${src_path}/*
+do  
+    temp_file=`basename $file  .pdf`  
+    ./pdffigures -j $dst_path/$temp_file $src_path/$file
+    echo $temp_file
+done
